84d5a1c3e0eac67573a08c00af81a2f0bec52f9c
Joshua Olazo <Joshuaolazo@gmail.com>
2019-04-20 17:35:18 -0400

    Update Gradebook.js

--- client/src/teacher/components/Gradebook.js
+++ client/src/teacher/components/Gradebook.js
@@ -48,19 +48,6 @@ const styles = theme => ({
   },
 });
 
-let id = 0;
-function createData(name, calories, fat, carbs, protein) {
-  id += 1;
-  return { id, name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
 
 
 
@@ -135,7 +122,7 @@ class Gradebook extends Component{
                       <TableHead>
                         <TableRow>
                           <TableCell>Students</TableCell>
-                          {classInfo.assignments.map( (name,key) => (
+                          {classInfo.assignments.map( (name) => (
                             <TableCell align="right">{name}</TableCell>
                           ))}
                         </TableRow>
@@ -146,8 +133,8 @@ class Gradebook extends Component{
                             <TableCell component="th" scope="row">
                               {name}
                             </TableCell>
-                            {classInfo.grades[key].map( (grade,key) => (
-                              <TableCell align="right">{grade}</TableCell>
+                            {classInfo.assignments.map( (assignment, key2) => (                             
+                              <TableCell align="right">{classInfo.grades[key][key2]}</TableCell>
                             ))}
                           </TableRow>
                         ))}
@@ -165,6 +152,5 @@ class Gradebook extends Component{
 Gradebook.propTypes = {
   sty: PropTypes.object.isRequired,
 };
-
 
 export default  withStyles(styles)(Gradebook);
