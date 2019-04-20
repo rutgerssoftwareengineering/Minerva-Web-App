d56e2c9e69de67d91c2caaaee1c0fbef1aecd034
MBrianLoc <M.BrianLoc@yahoo.com>
2019-04-20 04:25:29 -0400

    working file upload and delete

--- client/src/teacher/containers/TeacherApp.js
+++ client/src/teacher/containers/TeacherApp.js
@@ -12,21 +12,36 @@ import ViewAnnouncement from '../components/ViewAnnouncement';
 import history from '../../History';
 import ForumContainer from './ForumContainer'
 import RegisterClass from '../components/RegisterClass'
-import manageFiles from './manageFiles'
+import ManageFiles from './ManageFiles'
 
 
 class TeacherApp extends Component {
     constructor(props){
         super(props)
         this.state={
-            quizzesData: [],
+            files: [],
             isOpen: false
     }}
+    componentWillMount() {
+        this.getFiles();
+    }
     toggleRegister = () => {
         this.setState({
           isOpen: !this.state.isOpen
         })
-      }
+    }
+    getFiles() {
+        fetch('/api/getFiles')
+          .then(res => res.json())
+          .then(files => {
+            if (files.message) {
+              console.log('No Files');
+              this.setState({ files: [] })
+            } else {
+              this.setState({ files: files})
+            }
+          });
+    }
     render(){
     return(
         <Provider store = {store}>
@@ -40,7 +55,7 @@ class TeacherApp extends Component {
                 <Route exact path='/createQuiz' component={CreateQuiz} />
                 <Route exact path='/announcements/new' component={CreateAnnouncement} />
                 <Route exact path='/announcements/view' component={ViewAnnouncement} />
-                <Route exact path='/manageFiles' component={manageFiles} />
+                <Route path='/manageFiles' render={(routerProps) => <ManageFiles {...routerProps} files={this.state.files}/>}/>
                 <RegisterClass className='navButton'show={this.state.isOpen} onClose={this.toggleRegister}>
                         Register a new class
                 </RegisterClass>
