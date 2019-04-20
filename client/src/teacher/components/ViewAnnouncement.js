7c80c7c03adbe36fc3920d430c765720359eac14
Joshua Olazo <Joshuaolazo@gmail.com>
2019-04-19 23:00:54 -0400

    quiz view spacing form top fix

--- client/src/teacher/components/ViewAnnouncement.js
+++ client/src/teacher/components/ViewAnnouncement.js
@@ -49,6 +49,7 @@ class ViewAnnouncement extends Component{
                 justify="center"
                 style={{ minHeight: '100vh' }}
             >
+                
                 <h1>Announcements</h1>
                 <div>
                      <NavLink to="/announcements/new" className="Link" >
