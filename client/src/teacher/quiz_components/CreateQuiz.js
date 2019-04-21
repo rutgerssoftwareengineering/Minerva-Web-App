4467a1e94428c6add99ecd652077bfcc49a0dd55
salman-omer <salman@DESKTOP-T2MHE68.localdomain>
2019-04-21 18:09:43 -0400

    open ended answer bug resolved and quiz title moved down

--- client/src/teacher/quiz_components/CreateQuiz.js
+++ client/src/teacher/quiz_components/CreateQuiz.js
@@ -282,6 +282,7 @@ class CreateQuiz extends Component {
             >
             <div>
                 <form>
+                    <br/><br/><br/><br/>
                     <label>
 
                         <TextField
@@ -291,15 +292,6 @@ class CreateQuiz extends Component {
                         value={this.state.changeQuizTitle}
                         onChange={this.changeQuizTitle}
                         />
-                        {/* <h1>Quiz Title:
-                        <input 
-                                type="text" 
-                                onChange={this.changeQuizTitle}
-                                value={this.state.quizTitle}
-                                style={{ width:"300px", marginLeft:"10px"}}
-                        />
-                        </h1>
-                        */}
                        <br/><br/> 
                     </label>
                      <label>
